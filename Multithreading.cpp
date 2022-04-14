dbe3c0fb3ef7416d9cc1a572ba007c4ba7531d83
xuronghua2001 <xuronghua2001@163.com>
2022-04-14 23:41:34 +0800

    Update Multithreading.cpp

--- Multithreading.cpp
+++ Multithreading.cpp
@@ -11,8 +11,8 @@ void MyThread::myTimeout()
   while(isStop == false)
   {
     id++;
-    QThread::sleep(3);//强制暂停3秒，使得每三秒产生一次信号，
     emit mySignal();//每3秒发送一次信号
+    QThread::sleep(3);//强制暂停3秒，使得每三秒产生一次信号，
     //qDebug()<<"子线程号"<<QThread::currentThread();
   }
 }
